f5edd8330ab215d79de73065d662a169f2b1db6e
Ramaebebe <127149219+Ramaebebe@users.noreply.github.com>
2025-09-23 23:57:13 +0200

    chore: ignore .next

--- .next/static/chunks/app/page-605c1a83b42ddaca.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{21884:function(n,e,u){Promise.resolve().then(u.t.bind(u,72972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=21884)}),_N_E=n.O()}]);
